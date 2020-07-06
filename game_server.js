2e6bd04b8412c215b35ad03a15cc53f89a0cd048
hyeonQyu <dhk0561@naver.com>
2020-07-06 14:10:56 +0900

    server update

--- game_server.js
+++ game_server.js
@@ -271,9 +271,10 @@ io.on('connection', function(socket) {
             }
         }
         console.log(disconnectedIndex+"player is disconnected in "+ port+' '+socket.id);
+        SCORE_BOARD[disconnectedIndex] = -1;
         CONNECTED_CLIENT_COUNT -= 1;
 
-        if(CONNECTED_CLIENT_COUNT == 1){
+        if(!isEnd && CONNECTED_CLIENT_COUNT == 1){
             var winnerIndex = 0;
             for(var j = 1; j < totalPlayer; j++){
                 if(SCORE_BOARD[j] < SCORE_BOARD[winnerIndex]){
